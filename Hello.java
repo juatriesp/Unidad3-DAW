2761e774acb7988c6bd7d8a97de8276d4d843e6e
juatriesp <jtriesp073@iesalmudeyne.es>
2024-10-02 09:37:59 +0200

    Tercer commit

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 //Hello.java
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World! from GIT");
+        System.out.println("Hola Mundo! Desde GIT");
     }
 }
